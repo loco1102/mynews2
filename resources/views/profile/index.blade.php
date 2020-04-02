7ed6a35e1c12ee107f7402b3c94e6e60a2f0c863
loco1102 <ryuhei1102@gmail.com>
2020-04-02 17:18:55 +0000

    profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.front')
+@extends('layouts.profilefront')
 
 @section('content')
     <div class="container">
@@ -9,18 +9,19 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
-                                <div class="title p-2">
+                                <div class="name p-2">
                                     <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                </div>
+                                <div class="gender p-2">
+                                    <h1>{{ str_limit($headline->title, 50) }}</h1>
+                                </div>
+                                <div class="hobby p-2">
+                                    <h1>{{ str_limit($headline->title, 50) }}</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                            <p class="introduction mx-auto">{{ str_limit($headline->body, 650) }}</p>
                         </div>
                     </div>
                 </div>
@@ -36,17 +37,18 @@
                                 <div class="date">
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
-                                <div class="title">
+                                <div class="name">
                                     {{ str_limit($post->title, 150) }}
                                 </div>
-                                <div class="body mt-3">
+                                <div class="gender">
+                                    {{ str_limit($post->title, 100) }}
+                                </div>
+                                <div class="hobby">
+                                    {{ str_limit($post->title, 100) }}
+                                </div>
+                                <div class="introduction mt-3">
                                     {{ str_limit($post->body, 1500) }}
                                 </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
                             </div>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('/', 'NewsController@index');
 });
 
 //kadai4
@@ -37,4 +38,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
     Route::get('profile', 'Admin\ProfileController@index')->middleware('auth');
     Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
+    Route::get('/profile', 'ProfileController@index');
 });
